bbda01c9cd80d1afef705674fe05fcf7b73dfab9
Jeff Milner <j.milner359@gmail.com>
2021-05-17 13:15:09 -0700

    Move the todo to it's own file

---
+++ TODO.md
@@ -0,0 +1,9 @@
+## Todo list
+
+- Work out keywords to automate part of the conditions,
+    - HC & DJ means start with vg/vg, pb means vg in wraps, etc
+
+- Work out filling in keywords "--cat mys" or something to default the
+  normal mystery keywords and so on?
+
+- Make it so if you select a pub from the list, it doesn't ask again if you wanna use it
--- index.js
+++ index.js
@@ -82,16 +82,6 @@ if (!isbn || (isbn.length !== 10 && isbn.length !== 13)) return console.log("Inv
 
 const API_URL = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
 
-// TODO-list
-//
-// - Work out keywords to automate part of the conditions,
-//     - HC & DJ means start with vg/vg, pb means vg in wraps, etc
-//
-// - Work out filling in keywords "--cat mys" or something to default the
-//   normal mystery keywords and so on?
-//
-// - Make it so if you select a pub from the list, it doesn't ask again if you wanna use it
-
 let jsonOut = null;
 async function init() {
     await fetch(API_URL)
