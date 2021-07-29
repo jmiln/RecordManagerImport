5a1ca7c320ce259db38a32f7496fba2f54dc4712
Jeff Milner <j.milner359@gmail.com>
2021-07-29 11:49:26 -0700

    Data update

---
+++ data/condLocs.js
@@ -0,0 +1,66 @@
+exports.cond = {
+    // Something special
+    sig: "Signed by author.",
+    ins: "Inscribed to prev. owner.",
+    sigins: "Inscribed & Signed by author.",
+    siginsp: "Inscribed to Prev. Owner & Signed by author.",
+
+    // Remainders
+    red: "Remainder Dot.",
+    remd: "Remainder Dot.",
+    rem: "Remainder Mark.",
+    remm: "Remainder Mark.",
+
+    // Edgewear
+    few:  "Faint Edgewear.",
+    lew:  "Light Edgewear.",
+
+    // General Wear
+    fw:  "Faint Wear.",
+    lw:  "Light Wear.",
+
+    lp:  "Large Print.",
+
+    // Previous owner stuff
+    poad:  "Prev. Owner's address label inside.",
+    poin:  "Prev. Owner's initials inside.",
+    poins: "Prev. Owner's inscription inside.",
+    pona:  "Prev. Owner's name inside.",
+    pono:  "Prev. Owner note inside.",
+    pobp:  "Prev. Owner bookplate inside.",
+
+    // Toning
+    fton: "Faint toning to page edges.",
+    lton: "Light toning to page edges.",
+    ton:  "Toning to page edges.",
+
+    // Soiling to page edges
+    fsoi: "Faint soiling to page edges.",
+    lsoi: "Light soiling to page edges.",
+
+    // Board damage
+    fewb: "Faint Edgewear to Boards.",
+    lewb: "Light Edgewear to Boards.",
+
+    // Boards & DJ
+    fewbd: "Faint Edgewear to Boards & DJ.",
+    lewbd: "Light Edgewear to Boards & DJ.",
+
+    // DJ stuff
+    fewd: "Faint Edgewear to DJ.",
+    lewd: "Light Edgewear to DJ.",
+    fwd: "Faint Wear to DJ.",
+    lwd: "Light Wear to DJ.",
+    djm: "DJ in Mylar Wrap.",
+    myl: "DJ in Mylar Wrap.",
+};
+
+exports.locs = {
+    fwr: "front wrap",
+    rwr: "rear wrap",
+    dj:  "dj",
+    ffep: "ffep",
+    djrf: "dj rear flap",
+    djff: "dj front flap",
+
+}
