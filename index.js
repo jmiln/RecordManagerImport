30fdc44f9403e796b7c537ca6ad46146bebf299b
Jeff Milner <j.milner359@gmail.com>
2022-02-03 12:56:43 -0800

    Update node-fetch to 3.1.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
 const fs = require("fs");
 const { inspect } = require("util");
 const { exec } = require("child_process");
